2eec73dbfac7fb10e527ab5cf8748c15319c9be6
Pritam Kadam <kpritam@thoughtworks.com>
2021-05-12 19:05:07 +0530

    Remove unused files

--- pull.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Pulling sequencer-scripts"
-git -C sequencer-scripts pull
-
-echo "Pulling esw-ocs-eng-ui"
-git -C esw-ocs-eng-ui pull
--- src/main/scala/dev/apps/TMTRunner.scala
+++ src/main/scala/dev/apps/TMTRunner.scala
@@ -1,6 +1,6 @@
 package dev.apps
 
-import caseapp.{AppName, CommandApp, HelpMessage, ProgName, RemainingArgs}
+import caseapp.{CommandApp, ExtraName, HelpMessage, RemainingArgs}
 import dev.utils.Git
 
 sealed trait Command
@@ -9,7 +9,7 @@ sealed trait Command
 case object Init extends Command
 
 @HelpMessage("Starts csw-services and esw eng-ui-services")
-case class Start(live: Boolean = false) extends Command
+case class Start(@ExtraName("l") live: Boolean = false) extends Command
 
 @HelpMessage("Updates all submodules to their correct versions")
 case object UpdateSubmodules extends Command
