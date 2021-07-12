7bd30abfc7d4060f57e60b3dc54703c9cdaa0c7b
Pritam Kadam <kpritam@thoughtworks.com>
2021-07-12 15:03:20 +0530

    Fix tagged version reading code and upgrade versions

--- src/main/scala/dev/utils/AppConfig.scala
+++ src/main/scala/dev/utils/AppConfig.scala
@@ -8,9 +8,18 @@ object AppConfig {
   private lazy val scriptsBuildProp = parse(Submodule.SequencerScripts)
   private lazy val eswBuildProp     = parse(Submodule.ESW)
 
-  lazy val ScriptsVersion: String = appConfig.getString("scripts.version")
-  lazy val EswVersion: String     = scriptsBuildProp.getString("esw.version")
-  lazy val CswVersion: String     = eswBuildProp.getString("csw.version")
+  lazy val ScriptsVersion: String = readVersion(appConfig.getString("scripts.version"))
+  lazy val EswVersion: String     = readVersion(scriptsBuildProp.getString("esw.version"))
+  lazy val CswVersion: String     = readVersion(eswBuildProp.getString("csw.version"))
 
   private def parse(submodule: Submodule) = ConfigFactory.parseFile(submodule.buildProperties.toIO)
+
+  // Assumptions:
+  // 1. Version Scheme: x.y.z
+  // 2. sha does not contains '.'
+  private def isTagged(sha: String) = sha.split('.').length >= 2
+
+  private def readVersion(sha: String) =
+    if (isTagged(sha)) "v" + sha
+    else sha
 }
